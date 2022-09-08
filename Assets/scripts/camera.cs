3adb327990a294af89ed5d2fd382fba2d66ddb54
Wyrei <r.w.kossen@gmail.com>
2022-09-08 13:00:56 +0200

    lopen en schieten. inzoemen damage

--- Assets/scripts/camera.cs
+++ Assets/scripts/camera.cs
@@ -8,10 +8,11 @@ public class camera : MonoBehaviour
     public Transform player;
 
     float xRotation = 0f;
+    public GameObject gun;
     // Start is called before the first frame update
     void Start()
     {
-        //Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -23,5 +24,17 @@ public class camera : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         player.Rotate(Vector3.up * mouseX);
+        if (Input.GetMouseButtonDown(1))
+        {
+            Camera.main.fieldOfView = 30;
+            gun.SetActive(false);
+            mouseS -= 500;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            Camera.main.fieldOfView = 50;
+            gun.SetActive(true);
+            mouseS += 500;
+        }
     }
 }
---
+++ Assets/scripts/gun.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+    public float damege = 10f;
+    public float range = 100f;
+    public float impactforce = 30f;
+    public float firerate = 15f;
+
+    public Camera fpscam;
+    public ParticleSystem muzzleflash;
+    public GameObject impacteff;
+
+    private float nexttimetofire = 1f;
+    public GameObject sniper;
+    public GameObject handgun;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Time.time >= nexttimetofire )
+        {
+            nexttimetofire = Time.time + 1f / firerate;
+            shoot();
+        }
+        if(Input.GetKeyDown("1"))
+        {
+            handgun.SetActive(false);
+            sniper.SetActive(true);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            handgun.SetActive(true);
+            sniper.SetActive(false);
+        }
+
+
+    }
+    void shoot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            muzzleflash.Play();
+
+            RaycastHit hit;
+            if (Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit, range))
+            {
+                Debug.Log(hit.transform.name);
+                targe target = hit.transform.GetComponent<targe>();
+                if (target != null)
+                {
+                    target.takedamage(damege);
+                }
+
+                if (hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(-hit.normal * impactforce);
+                }
+
+                GameObject impactGO = Instantiate(impacteff, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impactGO, 2f);
+            }
+        }
+        
+
+    }
+}
